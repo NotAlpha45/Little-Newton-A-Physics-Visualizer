555b24d132e87c0ed69a7a927e0f71023387077b
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2021-05-30 00:44:06 +0600

    Update gravity and velocity.js

--- gravity and velocity.js
+++ gravity and velocity.js
@@ -1,13 +1,16 @@
 let body;
 
 function setup() {
-  createCanvas(600, 600).position(100);
-  frameRate(60);
-  body = new Mover(300, 100, 40);
+  createCanvas(1360, 600).position(0);
+  frameRate(20);
+  body = new Mover(100, 700 - 40, 40);
+  body.setHeight(600);
+  body.setVelocity(30, 0);
 }
 function draw() {
   background(94, 219, 211);
   body.update();
   body.checkBound();
   body.display();
+  console.log("Looping");
 }
