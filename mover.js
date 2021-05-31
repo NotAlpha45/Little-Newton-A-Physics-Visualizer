68452e5b96814538f8c849e01163328910453e16
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2021-05-31 23:36:29 +0600

    Implementing method for drawing trail.

--- mover.js
+++ mover.js
@@ -4,7 +4,9 @@ class Mover {
     this.radius = radius;
     this.velocity = createVector(0, 0);
     this.gravity = createVector(0, 1);
-    this.damping = -0.6;
+    this.damping = -0.7;
+    // Previous position of the object. This will help us in drawing trails.
+    this.previous_position = createVector(-1, -1);
   }
 
   // Set the initial velocity and the angle of that velocity.
@@ -13,6 +15,7 @@ class Mover {
     this.velocity.y = value * Math.sin(angle);
   }
 
+  // Sets the hight of the object from the ground.
   setHeight(value) {
     this.position.y = height - this.radius - value;
   }
@@ -22,8 +25,6 @@ class Mover {
     if (this.position.y > height - this.radius) {
       this.position.y = height - this.radius;
       this.velocity.y *= this.damping;
-      // this.velocity.y = 0;
-      // this.velocity.x = 0;
     } else if (this.position.y < this.radius) {
       this.position.y = this.radius;
       this.velocity.y *= this.damping;
@@ -44,10 +45,27 @@ class Mover {
     this.position.add(this.velocity);
   }
 
-  //Displays the object
+  // Draws the trail of the object.
+  draw_trail(buffer) {
+    buffer.stroke(0);
+    // frameCount is anothe builtin attribute that keeps track of frames.
+    if (frameCount > 1) {
+      // Draws a line on the buffer screen from the previous position to the current position
+      // which acts like a trail.
+      buffer.line(
+        this.previous_position.x,
+        this.previous_position.y,
+        this.position.x,
+        this.position.y
+      );
+    }
+    this.previous_position.x = this.position.x;
+    this.previous_position.y = this.position.y;
+  }
+
+  //Displays the object, which is an ellipse
   display() {
-    // stroke(25);
-    strokeWeight(0);
+    noStroke();
     fill(255, 100, 100);
     ellipse(this.position.x, this.position.y, this.radius * 2);
   }
