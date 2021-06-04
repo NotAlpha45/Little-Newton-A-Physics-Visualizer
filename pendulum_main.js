80e4980758f8bd7544ea25bf457f2461dbcb344a
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2021-06-04 06:21:53 +0600

    Adding graphing mechanism

--- pendulum_main.js
+++ pendulum_main.js
@@ -1,13 +1,22 @@
 let bob;
+let frames;
+let x_pos = [];
 function setup() {
   canvas = createCanvas(800, 600);
   canvas.position(0);
+  canvas.parent("pendulum_simulation");
   frameRate(60);
   buffer = createGraphics(width, height);
   buffer.background(94, 219, 211);
-  bob = new Bob(300, Math.PI/6, 30);
-  bob.set_damping(0.9995);
+  bob = new Bob(300, Math.PI / 6, 30);
+  bob.set_damping(1);
 
+  Plotly.plot("chart", [
+    {
+      y: [bob.position.x],
+      type: "line",
+    },
+  ]);
 }
 
 function draw() {
@@ -16,6 +25,21 @@ function draw() {
   // image() will blit the buffer screen on the main screen like a rectangle.
   image(buffer, 0, 0, width, height);
   bob.display();
-//   bob.draw_trail(buffer);
+  //   bob.draw_trail(buffer);
   bob.update();
+  frames = frameCount;
 }
+
+// Sets interval for drawing of the graph. Takes the function to be intervaled and
+// the interval time as parameters. The extendTraces() functions extends a previously
+// drawn function. In this case, it extends the graph drawn on the "chart element"  
+setInterval(function () {
+
+  Plotly.extendTraces(
+    "chart",
+    {
+      y: [[bob.position.x]],
+    },
+    [0]
+  );
+}, 10);
