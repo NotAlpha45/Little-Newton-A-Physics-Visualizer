b5aff57f91276d46ff39d4fee0a5369fd5da0f98
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2021-07-04 06:48:23 +0600

    Added input functionality and divisions.

--- gravity and velocity.js
+++ gravity and velocity.js
@@ -1,17 +1,99 @@
 let body;
+let buffer;
+let body_height, height_input_field, height_text;
+let angle, angle_input_field, angle_text;
+let initial_velocity, initial_velocity_input_field, initial_velocity_text;
+let gravity, gravity_input_field, gravity_text;
+let button;
 
+function angle_input_maker() {
+  angle_text = createElement("h3", "Angle (θ): ");
+  angle_text.position(width - 250, 100);
+  angle_text.parent("projectile_simulation");
+  angle_input_field = createInput("30");
+  angle_input_field.position(width - 150, 120);
+  angle_input_field.size(50);
+  angle_input_field.parent("projectile_simulation");
+}
+
+function velocity_input_maker() {
+  initial_velocity_text = createElement("h3", "Initial Velocity (v₀): ");
+  initial_velocity_text.position(width - 327, 160);
+  initial_velocity_text.parent("projectile_simulation");
+  initial_velocity_input_field = createInput("20");
+  initial_velocity_input_field.position(width - 150, 180);
+  initial_velocity_input_field.size(50);
+  initial_velocity_input_field.parent("projectile_simulation");
+}
+
+function height_input_maker() {
+  body_height_text = createElement("h3", "Height (h): ");
+  body_height_text.position(width - 257, 130);
+  body_height_text.parent("projectile_simulation");
+  height_input_field = createInput("20");
+  height_input_field.position(width - 150, 150);
+  height_input_field.size(50);
+  height_input_field.parent("projectile_simulation");
+}
+
+function gravity_input_maker() {
+  gravity_text = createElement("h3", "Gravity (g):");
+  gravity_text.position(width - 265, 190);
+  gravity_text.parent("projectile_simulation");
+  gravity_input_field = createInput("9.8");
+  gravity_input_field.position(width - 150, 210);
+  gravity_input_field.size(50);
+  gravity_input_field.parent("projectile_simulation");
+}
+
+function button_maker(parent, posx, posy, label, func) {
+  button = createButton(label);
+  button.position(posx, posy);
+  button.mousePressed(func);
+  button.parent(parent);
+}
 // Sets up the screen.
 function setup() {
   canvas = createCanvas(1360, 600);
   canvas.position(0);
+  canvas.parent("projectile_simulation");
   frameRate(60);
-  body = new Mover(100, 700 - 40, 40);
-  body.setHeight(20);
-  body.setVelocity(40, Math.PI/3);
-
   // buffer is a secondary surface that we will draw our trail on.
   buffer = createGraphics(width, height);
   buffer.background(94, 219, 211);
+
+  body = new Mover(100, 700 - 31, 20);
+
+  angle_input_maker();
+
+  height_input_maker();
+
+  velocity_input_maker();
+
+  gravity_input_maker();
+
+  button_maker("projectile_simulation", width - 150, 240, "Run", value_input);
+  button_maker("projectile_simulation", width - 150, 270, "Reset", setup);
+}
+
+function value_input() {
+  initial_velocity = float(initial_velocity_input_field.value());
+
+  gravity = float(gravity_input_field.value()) / 9.8;
+
+  body_height = float(height_input_field.value()) * 10;
+
+  if (float(angle_input_field.value) == 90) {
+    angle = Math.PI / 2;
+  } else {
+    angle = (float(angle_input_field.value()) * Math.PI) / 180;
+  }
+
+  body.setHeight(body_height);
+
+  body.setVelocity(initial_velocity, angle);
+
+  body.setGravity(gravity);
 }
 
 // Draws all the objects in a loop.
--- mover.js
+++ mover.js
@@ -4,30 +4,56 @@ class Mover {
     this.radius = radius;
     this.velocity = createVector(0, 0);
     this.gravity = createVector(0, 1);
+    // Note, damping must be between 0 and 1 and be negative
+    // The higher the friction, the more friction will be simulated.
     this.damping = -0.7;
-    // Previous position of the object. This will help us in drawing trails.
-    this.previous_position = createVector(-1, -1);
-  }
+    this.friction = 2;
 
-  // Set the initial velocity and the angle of that velocity.
-  setVelocity(value, angle) {
-    this.velocity.x = value * Math.cos(angle);
-    this.velocity.y = value * Math.sin(angle);
+    // Previous position of the object. This will help us in drawing trails.
+    this.previous_position = createVector(x, y);
   }
 
   // Sets the hight of the object from the ground.
   setHeight(value) {
     this.position.y = height - this.radius - value;
+    this.previous_position.y = this.position.y;
+  }
+
+  // Set the initial velocity and the angle of that velocity.
+  setVelocity(value, angle) {
+    this.velocity.x = value * Math.cos(angle);
+    //Since velocity is facing upwards
+    this.velocity.y = -value * Math.sin(angle);
+  }
+
+  setGravity(value) {
+    this.gravity.y = value;
+  }
+
+  applyFriction() {
+    if (Math.abs(this.velocity.x) <= this.friction) {
+      this.velocity.x = 0;
+      return;
+    }
+    if (this.velocity.x > 0) {
+      this.velocity.x -= this.friction;
+    } else if (this.velocity.x < 0) {
+      this.velocity.x += this.friction;
+    }
   }
 
   // Checks if object has hit the boundaries and then implements reaction accordingly
   checkBound() {
+    // If the object is on the ground.
     if (this.position.y > height - this.radius) {
       this.position.y = height - this.radius;
       this.velocity.y *= this.damping;
+      this.applyFriction();
+      // this.velocity.x *= this.damping;
     } else if (this.position.y < this.radius) {
       this.position.y = this.radius;
       this.velocity.y *= this.damping;
+      // this.velocity.x *= this.damping;
     }
 
     if (this.position.x > width - this.radius) {
@@ -48,10 +74,10 @@ class Mover {
   // Draws the trail of the object.
   draw_trail(buffer) {
     buffer.stroke(0);
-    // frameCount is anothe builtin attribute that keeps track of frames.
-    if (frameCount > 1) {
-      // Draws a line on the buffer screen from the previous position to the current position
-      // which acts like a trail.
+
+    // Draws a line on the buffer screen from the previous position to the current position
+    // which acts like a trail.
+    if (frameCount > 2) {
       buffer.line(
         this.previous_position.x,
         this.previous_position.y,
@@ -59,6 +85,7 @@ class Mover {
         this.position.y
       );
     }
+
     this.previous_position.x = this.position.x;
     this.previous_position.y = this.position.y;
   }
